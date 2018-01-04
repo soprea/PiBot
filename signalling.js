baf49d6cfc6907d03717bc8b3ef371229968ed9a
soprea <stefanoprea86@gmail.com>
2018-01-04 18:08:16 +0000

    uv4l

---
+++ signalling.js
@@ -0,0 +1,143 @@
+RTCPeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+RTCSessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
+RTCIceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+
+function signal(url, onSuccess, onError, onClose, onMessage) {
+    if ("WebSocket" in window) {
+        console.log("opening web socket: " + url);
+        var ws = new WebSocket(url);
+        var pc;
+
+        ws.onopen = function () {
+            /* First we create a peer connection */
+            var config = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]};
+            var options = {optional: []};
+            pc = new RTCPeerConnection(config, options);
+
+            pc.onicecandidate = function (event) {
+                if (event.candidate) {
+                    var candidate = {
+                        sdpMLineIndex: event.candidate.sdpMLineIndex,
+                        sdpMid: event.candidate.sdpMid,
+                        candidate: event.candidate.candidate
+                    };
+                    var request = {
+                        what: "addIceCandidate",
+                        data: JSON.stringify(candidate)
+                    };
+                    ws.send(JSON.stringify(request));
+                } else {
+                    console.log("end of candidates.");
+                }
+            };
+
+            pc.onaddstream = function (event) {
+                onSuccess(event.stream);
+            };
+
+            pc.onremovestream = function (event) {
+                console.log("the stream has been removed: do your stuff now");
+            };
+
+            pc.ondatachannel = function (event) {
+                console.log("a data channel is available: do your stuff with it");
+            };
+
+            /* kindly signal the remote peer that we would like to initiate a call */
+            var request = {
+                what: "call",
+                options: {
+                    force_hw_vcodec: true,
+                    vformat: 30 /* 640x480 */
+                }
+            };
+            ws.send(JSON.stringify(request));
+        };
+
+        ws.onmessage = function (evt) {
+            var msg = JSON.parse(evt.data);
+            var what = msg.what;
+            var data = msg.data;
+
+            console.log("message =" + what);
+
+            switch (what) {
+                case "offer":
+                    var mediaConstraints = {
+                        optional: [],
+                        mandatory: {
+                            OfferToReceiveAudio: true,
+                            OfferToReceiveVideo: true
+                        }
+                    };
+                    pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(data)),
+                            function onRemoteSdpSuccess() {
+                                pc.createAnswer(function (sessionDescription) {
+                                    pc.setLocalDescription(sessionDescription);
+                                    var request = {
+                                        what: "answer",
+                                        data: JSON.stringify(sessionDescription)
+                                    };
+                                    ws.send(JSON.stringify(request));
+                                }, function (error) {
+                                    onError("failed to create answer: " + error);
+                                }, mediaConstraints);
+                            },
+                            function onRemoteSdpError(event) {
+                                onError('failed to set the remote description: ' + event);
+                                ws.close();
+                            }
+                    );
+
+                    var request = {
+                        what: "generateIceCandidates"
+                    };
+                    ws.send(JSON.stringify(request));
+                    break;
+
+                case "answer":
+                    break;
+
+                case "message":
+                    if (onMessage) {
+                        onMessage(msg.data);
+                    }
+                    break;
+
+                case "iceCandidates":
+                    var candidates = JSON.parse(msg.data);
+                    for (var i = 0; candidates && i < candidates.length; i++) {
+                        var elt = candidates[i];
+                        let candidate = new RTCIceCandidate({sdpMLineIndex: elt.sdpMLineIndex, candidate: elt.candidate});
+                        pc.addIceCandidate(candidate,
+                                function () {
+                                    console.log("IceCandidate added: " + JSON.stringify(candidate));
+                                },
+                                function (error) {
+                                    console.error("addIceCandidate error: " + error);
+                                }
+                        );
+                    }
+                    break;
+            }
+        };
+
+        ws.onclose = function (event) {
+            console.log('socket closed with code: ' + event.code);
+            if (pc) {
+                pc.close();
+                pc = null;
+            }
+            if (onClose) {
+                onClose();
+            }
+        };
+
+        ws.onerror = function (event) {
+            onError("An error has occurred on the websocket (make sure the address is correct)!");
+        };
+
+    } else {
+        onError("Sorry, this browser does not support Web Sockets. Bye.");
+    }
+}
